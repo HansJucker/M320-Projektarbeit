f0a9125f48b1e21631326ada474b35c438ee7418
HansJucker <goeselsack13@gmail.com>
2024-01-11 22:22:11 +0100

    Test funktioniert

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -2,27 +2,29 @@
 using M320_Projektarbeit.Produkte;
 using NUnit.Framework;
 
-namespace M320ProjektarbeitTest
+namespace ClassLibrary1;
+
+
+[TestFixture]
+public class ProductTests
 {
-
-    [TestFixture]
-    public class ProductTests
+    [Test]
+    public void RemoveUnitsFromStock_ShouldDecreaseUnitsInStock()
     {
-        [Test]
-        public void RemoveUnitsFromStock_ShouldDecreaseUnitsInStock()
-        {
-            // Arrange
-            var supplier = new Supplier("Meier Import", "Hauptstrasse 3, Sursee");
-            var product = new Product("Tomaten",supplier , 100, 5, 12);
-            product.addUnitsToStock(10);
-            int initialStock = product.getUnitsInStock();
-            int unitsToRemove = 5;
+        // Arrange
+        var supplier = new Supplier("Meier Import", "Hauptstrasse 3, Sursee");
+        var product = new Product("Tomaten",supplier);
+        product.addUnitsToStock(10);
+        int initialStock = product.getUnitsInStock();
+        int unitsToRemove = 5;
+        int stockShouldBe = initialStock - unitsToRemove;
 
-            // Act
-            product.removeUnitsFromStock(unitsToRemove);
+        // Act
+        product.removeUnitsFromStock(unitsToRemove);
 
-            // Assert
-            Assert.That(initialStock - unitsToRemove == product.getUnitsInStock());
-        }
+        // Assert
+        Assert.That(initialStock - unitsToRemove == product.getUnitsInStock());     
+         
+        
     }
 }
