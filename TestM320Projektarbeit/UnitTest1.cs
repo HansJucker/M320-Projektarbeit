deabe73ad90bc591878d8bcc9439bb9e7237c59a
Louis Moeckli <Louis_Moeckli@sluz.ch>
2024-01-11 16:58:37 +0100

    added class library for unittest

---
+++ TestM320Projektarbeit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestM320Projektarbeit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
