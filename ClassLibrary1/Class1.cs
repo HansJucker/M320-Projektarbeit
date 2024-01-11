9e666846feba2dd4d2ac1648709f1f37e7c7880e
HansJucker <goeselsack13@gmail.com>
2024-01-11 21:54:29 +0100

    commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using M320_Projektarbeit;
+using M320_Projektarbeit.Produkte;
+using NUnit.Framework;
+
+namespace M320ProjektarbeitTest
+{
+
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void RemoveUnitsFromStock_ShouldDecreaseUnitsInStock()
+        {
+            // Arrange
+            var supplier = new Supplier("Meier Import", "Hauptstrasse 3, Sursee");
+            var product = new Product("Tomaten",supplier , 100, 5, 12);
+            product.addUnitsToStock(10);
+            int initialStock = product.getUnitsInStock();
+            int unitsToRemove = 5;
+
+            // Act
+            product.removeUnitsFromStock(unitsToRemove);
+
+            // Assert
+            Assert.That(initialStock - unitsToRemove == product.getUnitsInStock());
+        }
+    }
+}
---
+++ Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e4be964a-1928-4b67-a67c-41f62a0f8ff8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
